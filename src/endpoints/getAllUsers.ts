6d91f68c1ee5397b49b538c0892856767a56f2ed
Verônica S <veronicasgc30@gmail.com>
2022-10-05 22:08:47 -0300

    tabelas criadas

--- src/endpoints/getAllUsers.ts
+++ src/endpoints/getAllUsers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import connection from '../connection'
+import connection from '../database/connection'
 
 export const getAllUsers = async ( req: Request, res: Response ): Promise<void> => {
     //Endpoint apenas para teste de funcionamento da requisição no banco de dados
